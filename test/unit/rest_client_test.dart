d5b33fb011e31ffc7609524d5de8d18c21a788fa
Plague Fox <plugfox@gmail.com>
2022-09-11 01:54:22 +0400

    Add core feature image generation #2: add build url test

--- test/unit/rest_client_test.dart
+++ test/unit/rest_client_test.dart
@@ -22,8 +22,16 @@ void main() {
         equals(expectedPath),
       );
       expect(
+        RestClient.buildUri(Uri.parse('https://api.domain.tld'), 'mobile/v1/endpoint/'),
+        equals(expectedPath),
+      );
+      expect(
         RestClient.buildUri(Uri.parse('https://api.domain.tld/mobile/v1/endpoint'), ''),
         equals(expectedPath),
+      );
+      expect(
+        RestClient.buildUri(Uri.parse('https://api.domain.tld?param1=value1'), 'mobile/v1/endpoint?param2=value2'),
+        equals(expectedPath.replace(query: 'param1=value1&param2=value2')),
       );
     });
   });
